9da0de14e5df41716a2e109567d4be41d4abe8d4
LateButGood <97518544+LateButGood@users.noreply.github.com>
2022-03-04 15:41:19 +0900

    Update and rename S01_P01.js to S01_P01(Find minimum Number).js

--- S01_P01(Find minimum Number).js
+++ S01_P01(Find minimum Number).js
@@ -1,6 +1,6 @@
 const solution = function (a, b, c) {
     
-    let answer = 0;
+    let answer = 0;https://github.com/LateButGood/Coding_Test/blob/master/S01_P01.js
 
     if (a > b) answer = b;
     else answer = a;
